94a540ca3d35ff888ea22180c4b5b29db0c37371
FizzPang <pangmc@sctek.com>
2017-07-05 16:57:41 +0800

    private.js

--- routes/base.js
+++ routes/base.js
@@ -4,11 +4,7 @@
 
 module.exports=function(app){
 
-    //用户
-    app.use('/user',require(CONTROLLERS+'/user'));
     //首页
     app.use('/',require(CONTROLLERS+'/index'));
-    //计划
-    app.use('/plan',require(CONTROLLERS+'/plan'));
-
+   
 };
